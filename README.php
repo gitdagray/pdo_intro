ed338fff961b91b262a87a3c3ff6ac1cc3149647
gitdagray <DaveOnEleven@gmail.com>
2022-01-31 13:08:02 -0600

    update README

--- README.php
+++ README.php
@@ -6,6 +6,8 @@ A practice example for PHP PDO connections and querying a MySQL database with pr
 
 Use this code example to create your own PHP application that connects to MySQL and queries data from a table.
 
+This example uses the freely available for download "world" database. You will need to download and import it into your MySQL server. You can download the "world" database and other free databases [here](https://blog.sqlauthority.com/2020/02/15/mysql-download-sample-database-sakila-world-employee/).
+
 This is NOT an OOP example.
 
 ### Academic Honesty
